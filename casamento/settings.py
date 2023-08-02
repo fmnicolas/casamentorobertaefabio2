e3197594df2b671defab33064b159e8a0b5de3e5
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-08-01 21:43:46 -0300

    commit

--- casamento/settings.py
+++ casamento/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gewhufiwjeufkrh97whuejf$%*()671892e4981hh8rjpf19'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['casamentorobertaefabio2.vercel.app']
 
 
 # Application definition
