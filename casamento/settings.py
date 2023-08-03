9cd93f603e86e25d9896199020b3b1729f94a7a2
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-08-03 20:55:42 -0300

    commit

--- casamento/settings.py
+++ casamento/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'casamento.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'banco2'),
+        'USER': os.environ.get('DB_USER', 'nicolas2'),
+        'PASSWORD': os.environ.get('DB_PASS', 'senha'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
