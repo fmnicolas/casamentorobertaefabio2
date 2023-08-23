1b816267813571d8edae5bab37f9f5d80543a991
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-08-23 10:17:29 -0300

    commit

--- casamento/settings.py
+++ casamento/settings.py
@@ -15,7 +15,7 @@ import os
 import logging
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
