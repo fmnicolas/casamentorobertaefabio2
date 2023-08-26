439faaf53b680c4a5680b5d639966c8e984b099e
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-08-26 11:14:58 -0300

    commit

--- casamento/urls.py
+++ casamento/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rvsp.views import index
+from rvsp import websocket_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path('ws/', include(websocket_urlpatterns)),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
