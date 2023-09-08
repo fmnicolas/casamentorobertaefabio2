d94483d738ef90888bdad9b90add45b83d77f3aa
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-09-08 10:20:28 -0300

    commit

--- rvsp/models.py
+++ rvsp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Confirmacao(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=30)
