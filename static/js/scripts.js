8422aec0838f3bd7dc29a33676825c0ef90247dd
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-09-11 10:23:05 -0300

    commit

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -853,7 +853,7 @@ var Neela;
                 xhr.onreadystatechange = function () {
                     xhr.onreadystatechange = function () {
                         if (xhr.readyState === XMLHttpRequest.DONE) {
-                            if (xhr.status === 200) {
+                            if (xhr.status === 201) {
                                 const response = JSON.parse(xhr.responseText);
                                 if (response.success) {
                                     alert('Formulário enviado com sucesso!');
