bdc1fff3185d9a661f181bfa5ba6f7913b559a29
yTheEpicBoy <contato.ytheepicboy@gmail.com>
2023-09-13 19:53:03 -0300

    commit

--- rvsp/views.py
+++ rvsp/views.py
@@ -13,7 +13,7 @@ def verificar_email(request):
         form = ConfirmacaoForm(request.POST)
         if form.is_valid():
             form.save()
-            form.reset()
+            form = ConfirmacaoForm()
             return JsonResponse({'success': True})
     else:
         form = ConfirmacaoForm()
